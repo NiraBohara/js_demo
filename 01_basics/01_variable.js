fa4a0fd3feb1b324f4905340b37935f92e0f5cbb
NiraBohara <nirubohara84@gmail.com>
2025-06-13 02:18:51 +0000

    learn variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 4354
+let accountemail ="niru@232gmail.com"
+var accountPAssword = "45354"
+accountCity = "mnr" 
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+console.log(accountId);
+
+accountPAssword = "4354"
+accountemail ="keshab2gamil.com"
+accountCity = "dhn"
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountState, accountemail, accountPAssword,accountCity]);
+
+
