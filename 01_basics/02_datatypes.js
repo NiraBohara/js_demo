bdf87fcd63962ca60ca443fb89060443031a9b3b
NiraBohara <nirubohara84@gmail.com>
2025-06-13 03:01:49 +0000

    learn datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs , not browser
+console.log(3-3); console.log("niru"); // we use this types of code two console in one line but in between we use (;) then code is run otherwise we see the error
+
+
+/*console.log(3
++ ) not allowed */
+
+//  kinds of time alert  documantion if we need read to aleart we search in google (aleart mdn)
+
+// but details documentation we see in tc39.ec (ecma)
+
+let name= "niru"
+let age = 34
+let isLoggedIn = false
+
+/* Datatypes
+- number = 2 to the power 53
+- bigint
+- string = using in ""
+- boolelan = true/false
+true=1, false=0
+- null =standalone value
+- undefined = value not assign
+-symbols= for unique
+-object
+*/
+console.log(typeof age);
